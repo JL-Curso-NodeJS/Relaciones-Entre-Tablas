67a2432a46792072119c248b4ed41b63bf4fd3fc
OmgCopito95 <july1995bolt@gmail.com>
2022-09-11 19:49:27 -0300

    commit inicial a partir de estructura proyecto 2

---
+++ src/controllers/usuario.controller.js
@@ -0,0 +1,72 @@
+// FUNCIONAMIENTO DE TODAS LAS RUTAS DE USUARIO
+
+const models = require("../database/models/index");
+
+module.exports = {
+
+    listar: async (req, res) => {
+        try {
+            const users = await models.usuario.findAll()
+
+            res.json({
+                success: true,
+                data: {
+                    usuarios: users
+                }
+            })
+
+        } catch (err) {
+            return next(err)
+        }
+    },
+
+    listarInfo: async (req, res, next) => {
+        try {
+            const user = await models.usuario.findOne({
+                where: {
+                    id: req.params.idUsuario
+                }
+            })            
+
+            res.json({
+                success: true,
+                data: {
+                    usuario: user
+                }
+            })
+
+        } catch (err) {
+            return next(err)
+        }
+    },
+
+    crear: async (req, res) => {
+        try {
+            const user = await models.usuario.create(req.body)
+
+            res.json({
+                success: true,
+                data: {
+                    id: user.id
+                }
+            })
+
+        } catch (err) {
+            return next(err)
+        }
+    },
+
+    prueba: async (req, res) => {
+        try {
+            console.log('ejecutando prueba')
+
+            res.json({
+                message: "Hola mundo"
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+}
